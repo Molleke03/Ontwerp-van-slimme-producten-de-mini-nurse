691396b6f68a2b0c7f73fae7bc084cdc6547aea8
SamDeprez <152390104+SamDeprez@users.noreply.github.com>
2026-01-18 18:46:49 +0100

    Add files via upload

---
+++ codes/profiles.py
@@ -0,0 +1,75 @@
+ontmoeten_profiles = [
+    {
+        "name": "Natasha Rogers",
+        "age": 84,
+        "description": "Ik geniet van een goed gesprek en\nde rust van de natuur. Graag leer ik\nnieuwe mensen kennen die warmte,\nhumor en eenvoud waarderen."
+    },
+    {
+        "name": "Jan de Vries",
+        "age": 72,
+        "description": "Houdt van wandelen, klassieke \nmuziek en gezelligheid. Staat \nopen voor nieuwe vrienden en \ninteressante gesprekken."
+    },
+    {
+        "name": "Marianne Peters",
+        "age": 68,
+        "description": "Creatief en kunstzinnig. Liefhebber \nvan schilderen en literatuur. \nZoekt mensen om ervaringen \nen ideeën mee te delen."
+    },
+    {
+        "name": "Kees van Dijk",
+        "age": 76,
+        "description": "Sportief en actief. Fietst graag en\nspeelt schaak. Houdt van humor en\nmaatschappelijke gesprekken."
+    },
+    {
+        "name": "Elsa Jansen",
+        "age": 70,
+        "description": "Geniet van koken en tuinieren. Wil\nnieuwe mensen leren kennen die\npositief en sociaal zijn."
+    },
+]
+
+contacten_profiles = [
+    {
+        "name": "Hanna Vos",
+        "age": 81,
+        "description": "Vrolijk en empathisch. Houdt van\nbreien, kaartspellen en warme\ngesprekken over het leven."
+    },
+    {
+        "name": "Rob van Leeuwen",
+        "age": 74,
+        "description": "Liefhebber van geschiedenis en\npuzzels. Geniet van rustige \navonden en goed gezelschap."
+    },
+    {
+        "name": "Wim Scholten",
+        "age": 77,
+        "description": "Gezelligheidsmens. Houdt van\nkaarten, tuinieren en een stevige\nkop koffie met een goed verhaal."
+    },
+    {
+        "name": "Inge Bakker",
+        "age": 73,
+        "description": "Rustig en zorgzaam. Geniet van\nmuziek, vogels spotten en lichte\nwandelingetjes in de natuur."
+    },
+    {
+        "name": "Theo Hartman",
+        "age": 82,
+        "description": "Levensgenieter. Houdt van\nklassieke films en wandelen in de\nmorgenstilte."
+    },
+    {
+        "name": "Mila Brouwer",
+        "age": 67,
+        "description": "Open en creatief. Schrijft graag en\nmaakt foto’s van kleine momenten.\nZoekt fijne gesprekken."
+    },
+    {
+        "name": "Arie Koning",
+        "age": 75,
+        "description": "Eerlijk en nuchter. Tuiniert graag en\npraat met plezier over reizen en\nmooie herinneringen."
+    },
+    {
+        "name": "Sonja Willems",
+        "age": 79,
+        "description": "Warm en sociaal. Houdt van koken,\ngezelschapsspellen en rustige\nmiddagen met thee."
+    },
+    {
+        "name": "Leo Bosch",
+        "age": 71,
+        "description": "Sportief type. Wandelt dagelijks en\nluistert graag naar jazz. Houdt van\nrustige, oprechte gesprekken."
+    },
+]
